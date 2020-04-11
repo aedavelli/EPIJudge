1d05b234e408c6c66132393bbb8f93114c403b79
Vijaya Simha Reddy Aedavelli <9965118+aedavelli@users.noreply.github.com>
2020-04-11 18:10:35 +0530

    Test

--- epi_judge_cpp/euclidean_gcd.cc
+++ epi_judge_cpp/euclidean_gcd.cc
@@ -1,10 +1,19 @@
 #include "test_framework/generic_test.h"
 long long Gcd(long long x, long long y) {
-  // TODO - you fill in here.
-  return 0;
+  long long t = y;
+  x = x < y ? t = x, y : x;
+  y = x == y ? t : y;
+
+  while (y != 0) {
+    t = y;
+    y = x % y;
+    x = t;
+  }
+
+  return x;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
   return GenericTestMain(args, "euclidean_gcd.cc", "gcd.tsv", &Gcd,
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -1952,7 +1952,7 @@ problem_mapping = {
     "Chapter 15: Recursion": {
         "15.00 Bootcamp: Recursion": {
             "C++: euclidean_gcd.cc": {
-                "passed": 0,
+                "passed": 10034,
                 "total": 10034
             },
             "Java: EuclideanGcd.java": {
